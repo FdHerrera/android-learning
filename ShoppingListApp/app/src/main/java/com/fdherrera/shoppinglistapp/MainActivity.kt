543335de0604722ff2318e7be5ca435a30531947
Federico Herrera <federico.herrera_dev@outlook.com>
2024-11-26 08:47:31 -0300

    Add shopping items, move app

--- ShoppingListApp/app/src/main/java/com/fdherrera/shoppinglistapp/MainActivity.kt
+++ ShoppingListApp/app/src/main/java/com/fdherrera/shoppinglistapp/MainActivity.kt
@@ -4,15 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.fdherrera.shoppinglistapp.ui.theme.ShoppingListAppTheme
 
@@ -29,21 +23,6 @@ class MainActivity : ComponentActivity() {
                     ShoppingListApp()
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun ShoppingListApp() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center
-    ) {
-        Button(
-            onClick = {},
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        ) {
-            Text("Add Item")
         }
     }
 }
