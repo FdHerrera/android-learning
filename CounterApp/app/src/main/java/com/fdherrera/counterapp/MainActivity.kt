5c396a4eb4bc4e99061e6fab6c4785663b315715
Federico Herrera <federico.herrera_dev@outlook.com>
2024-11-26 13:12:17 -0300

    Add View from MVVM

--- CounterApp/app/src/main/java/com/fdherrera/counterapp/MainActivity.kt
+++ CounterApp/app/src/main/java/com/fdherrera/counterapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -15,10 +16,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -31,9 +28,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val viewModel by viewModels<CounterViewModel>()
             CounterAppTheme {
                 Surface {
-                    CounterApp()
+                    CounterApp(viewModel)
                 }
             }
         }
@@ -41,37 +39,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CounterApp() {
-    var counter by remember { mutableIntStateOf(0) }
-
-    fun increment() {
-        counter++
-    }
-
-    fun decrement() {
-        counter--
-    }
-
+fun CounterApp(viewModel: CounterViewModel) {
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Count: $counter",
+            text = "Count: ${viewModel.count.value}",
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold
         )
         Spacer(modifier = Modifier.height(16.dp))
         Row {
             Button(
-                onClick = { increment() },
+                onClick = { viewModel.increment() },
             ) {
                 Text("Increase")
             }
             Spacer(modifier = Modifier.width(16.dp))
             Button(
-                onClick = { decrement() },
+                onClick = { viewModel.decrement() },
             ) {
                 Text("Decrease")
             }
