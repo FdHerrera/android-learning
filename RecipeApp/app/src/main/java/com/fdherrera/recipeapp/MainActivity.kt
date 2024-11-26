81965ce6ce36d1047595b128a54132333dca078a
Federico Herrera <federico.herrera_dev@outlook.com>
2024-11-26 19:29:04 -0300

    Init recipe app

---
+++ RecipeApp/app/src/main/java/com/fdherrera/recipeapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.fdherrera.recipeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.Surface
+import com.fdherrera.recipeapp.ui.theme.RecipeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RecipeAppTheme {
+                Surface() { }
+            }
+        }
+    }
+}
